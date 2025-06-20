58eb5f05fc4e055cf3b36340b17276e43258c25c
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2025-06-20 18:40:25 +0530

    fixed test due to env issue

--- tests/UI/login.spec.js
+++ tests/UI/login.spec.js
@@ -10,56 +10,48 @@ test("Moralis UI", async ({ browser }) => {
 
     const context = await browser.newContext()
     const page = await context.newPage()
-
     await page.goto("https://admin.moralis.com/login")
-
     expect(page.url()).toContain("moralis")
-
     expect(await page.title()).toContain("Moralis for Developers")
-
 })
 
 
-test("Moralis Login", async ({ browser }) => {
-    const context = await browser.newContext()
-    const page = await context.newPage()
+// test("Moralis Login", async ({ browser }) => {
+//     const context = await browser.newContext()
+//     const page = await context.newPage()
 
-    await page.goto("https://admin.moralis.com/login")
+//     await page.goto("https://admin.moralis.com/login")
 
-    await page.locator("#admin-login-email").fill(process.env.EMAIL)
-    await page.waitForTimeout(2000);
-    await page.locator("span[title='Password']").fill(process.env.PASSWORD)
-    await page.waitForTimeout(2000);
-    await page.locator("[type='submit']").click()
-    await page.waitForTimeout(2000);
+//     await page.locator("#admin-login-email").fill(process.env.EMAIL)
+//     await page.waitForTimeout(2000);
+//     await page.locator("span[title='Password']").fill(process.env.PASSWORD)
+//     await page.waitForTimeout(2000);
+//     await page.locator("[type='submit']").click()
+//     await page.waitForTimeout(2000);
 
-    await page.pause()
+//     await page.pause()
 
-})
+// })
 
-test("Moralis Login POM without POManager", async ({ browser }) => {
-    const context = await browser.newContext()
-    const page = await context.newPage()
-    const loginpage = new Loginpage(page)
+// test("Moralis Login POM without POManager", async ({ browser }) => {
+//     const context = await browser.newContext()
+//     const page = await context.newPage()
+//     const loginpage = new Loginpage(page)
 
-    await page.goto("https://admin.moralis.com/login")
+//     await page.goto("https://admin.moralis.com/login")
 
-    loginpage.login(process.env.EMAIL, process.env.PASSWORD)
-    await page.pause()
+//     loginpage.login(process.env.EMAIL, process.env.PASSWORD)
+//     await page.pause()
 
-})
+// })
 
-test("Moralis Login POM", async ({ browser }) => {
-    const context = await browser.newContext()
-    const page = await context.newPage()
+test("Moralis Login POM with POManager", async ({ page }) => {
+    // const context = await browser.newContext()
+    //const page = await context.newPage()
     const poManager = new POManager(page)
-
     await page.goto("https://admin.moralis.com/login")
-
     const loginpage = poManager.getLoginPage()
+
     await loginpage.login(process.env.EMAIL, process.env.PASSWORD)
-
-
-    await page.pause()
 
 })
